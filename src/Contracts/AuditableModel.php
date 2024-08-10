70a7b1827cd755ddd541dea1d3912da74a8c5ebe
dje <diego.jaure@synolia.com>
2024-08-09 22:32:25 -0400

    adding migrations models and contracts

---
+++ src/Contracts/AuditableModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Eloise\DataAudit\Contracts;
+
+/**
+ * This a Contract for models only, every Model inside App\Models implementing this contract will be added to the audit_class table.
+ * If you are starting to use this Package you should use the Trait InitialDefaultActiveAuditTrait.
+ */
+interface AuditableModel
+{
+    public function getSourceModelClass(): string;
+
+    public function defaultAudit(): bool;
+    public function activeAudit(): bool;
+    public function versionAudit(): string;
+
+}
