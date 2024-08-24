81183008bbb7345dcbb13025486599292da33394
Diego Jaure <euler90@gmail.com>
2024-08-24 17:12:47 -0300

    Adding Rollback Feature #019

---
+++ src/Managers/RollbackManager.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Eloise\DataAudit\Managers;
+
+use Eloise\DataAudit\Constants\Actions;
+use Eloise\DataAudit\Constants\AuditableProperties;
+use Eloise\DataAudit\Contracts\AuditableModel;
+use Eloise\DataAudit\Queries\RollbackQuery;
+use Illuminate\Support\Carbon;
+
+class RollbackManager
+{
+    public function __construct(
+        protected AuditableModel $auditableModel,
+        protected Carbon|null $dateTime,
+    ) {
+    }
+
+    public function retrieve(): AuditableModel
+    {
+        $newModel = $this->auditableModel->replicate();
+
+        $oldModel = $this->retrieveDiff();
+        if ($oldModel === null) {
+            return $newModel;
+        }
+
+        foreach ($oldModel as $key => $value) {
+            if ($key == $this->auditableModel->getKeyName()) {
+                continue;
+            }
+            $newModel->setAttribute($key, $value);
+        }
+
+        return $newModel;
+    }
+
+    public function retrieveDiff(): array | null
+    {
+        $query = new RollbackQuery();
+        $audits = $query->getCollectionOfDiffs($this->auditableModel, $this->dateTime);
+
+        if ($audits->isEmpty()) {
+            return null;
+        }
+
+        if ($audits->count() === 1) {
+            return $audits->first()->diff;
+        }
+
+        $rollback = [];
+
+        foreach ($audits as $diffs) {
+            foreach ($diffs as $diff) {
+                $rollback[$diff[AuditableProperties::FIELD]]
+                    = ($diff[AuditableProperties::ORIGINAL_VALUE] === null)
+                    ? $diff[AuditableProperties::NEW_VALUE]
+                    : $diff[AuditableProperties::ORIGINAL_VALUE];
+            }
+        }
+
+        return $rollback;
+    }
+}
